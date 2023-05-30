94487fb6b4c9e74fdbec7dd9c9aeab15472dee1e
sola-grj <1131631886@qq.com>
2023-05-30 17:05:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>i am root</div>
+</template>
+
+<script setup lang="ts">
+const str = 'i am groot'
+const aa = { a: 1, b: 3 }
+</script>
+
+<style scoped>
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+}
+</style>
